8ee15228018620d91f5d01104e83dc1f7e5dbec4
Kiwi Bui <kiwibui4@gmail.com>
2021-06-21 02:58:18 -0700

    assignment complete

--- src/main/java/com/galvanize/Algorithm.java
+++ src/main/java/com/galvanize/Algorithm.java
@@ -1,6 +1,44 @@
 package com.galvanize;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Algorithm {
+    public boolean allEqual(String input) {
+        String stringLowerCase= input.toLowerCase();
 
+        for (int i = 0; i < stringLowerCase.length() - 1; i++) {
+            if (stringLowerCase.charAt(i) != (stringLowerCase.charAt(i + 1))) {
+                return false;
+            }
+        }
+
+        return !input.equals("");
+    }
+
+    public Map<String, Long> letterCount(String input) {
+        HashMap<String, Long> map = new HashMap<>();
+        String stringLowerCase= input.toLowerCase();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (!map.containsKey(String.valueOf(stringLowerCase.charAt(i)))) {
+                map.put(String.valueOf(stringLowerCase.charAt(i)), 1L);
+            } else {
+                map.put(String.valueOf(stringLowerCase.charAt(i)), map.get(String.valueOf(stringLowerCase.charAt(i))) + 1);
+            }
+        }
+
+        return map;
+    }
+
+    public String interleave(List<String> listOne, List<String> listTwo) {
+        String result = "";
+
+        for (int i = 0; i < listOne.size(); i++) {
+            result += listOne.get(i) + listTwo.get(i);
+        }
+
+        return result;
+    }
 }
--- src/test/java/com/galvanize/AlgorithmTest.java
+++ src/test/java/com/galvanize/AlgorithmTest.java
@@ -1,8 +1,40 @@
 package com.galvanize;
 
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AlgorithmTest {
+    Algorithm algorithm = new Algorithm();
 
+    @Test
+    void allEqualsTest() {
+        assertTrue(algorithm.allEqual("aAaaAAaaAaAa"), "Should return true for all characters equal");
+    }
 
+    @Test
+    void allEqualsFailsTest() {
+        assertFalse(algorithm.allEqual("aAbAaaAa"), "Should return true for all characters equal");
+    }
+
+    @Test
+    void mapTest() {
+        HashMap<String, Long> hashMap = new HashMap<>();
+
+        hashMap.put("a", 2L);
+        hashMap.put("b", 3L);
+        hashMap.put("c", 2L);
+
+        assertEquals(algorithm.letterCount("AabbbCC"), hashMap, "Should return hashmap of character count");
+    }
+
+    @Test
+    void interleaveTest() {
+        assertEquals(algorithm.interleave(Arrays.asList("a", "b", "c"), Arrays.asList("d", "e", "f")), "adbecf",
+                "Should return interleaved string");
+    }
 }
